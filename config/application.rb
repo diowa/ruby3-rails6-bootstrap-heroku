c311dd73fc75481b1d34797e216258089c420182
Geremia Taglialatela <tagliala.dev@gmail.com>
2022-03-23 09:00:12 +0100

    Fix module name and env variable

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ require 'action_cable/engine'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Ruby2Rails6BootstrapHeroku
+module Ruby3Rails6BootstrapHeroku
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
