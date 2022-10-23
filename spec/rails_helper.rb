8b95327be87ea70d0524506cd5418a4fd26a5026
Geremia Taglialatela <tagliala.dev@gmail.com>
2022-10-23 12:20:45 +0200

    Fix spec helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,20 +1,5 @@
 # frozen_string_literal: true
 
-require 'simplecov'
-
-SimpleCov.start 'rails' do
-  if ENV['CI']
-    require 'simplecov-lcov'
-
-    SimpleCov::Formatter::LcovFormatter.config do |c|
-      c.report_with_single_file = true
-      c.single_report_path = 'coverage/lcov.info'
-    end
-
-    formatter SimpleCov::Formatter::LcovFormatter
-  end
-end
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
