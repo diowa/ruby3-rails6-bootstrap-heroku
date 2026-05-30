7b114e3c9819cc0a8d5d43be829e9df56912d5cb
Geremia Taglialatela <tagliala.dev@gmail.com>
2026-05-30 17:11:06 +0200

    Update dependencies

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,30 +1,6 @@
-const { env, generateWebpackConfig, merge } = require('shakapacker')
-const { existsSync } = require('fs')
-const { resolve } = require('path')
-
-const webpack = require('webpack')
-
-let customConfig = {
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          { loader: 'resolve-url-loader' },
-          { loader: 'sass-loader', options: { sourceMap: true } }
-        ]
-      }
-    ]
-  }
-}
-
-const path = resolve(__dirname, `${env.nodeEnv}.js`)
-
-if (existsSync(path)) {
-  console.log(`Loading ENV specific webpack configuration file ${path}`)
-  const { envSpecificConfig } = require(path)
-  customConfig = merge(customConfig, envSpecificConfig)
-}
+// See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
+const { generateWebpackConfig } = require('shakapacker')
 
 const webpackConfig = generateWebpackConfig()
-module.exports = merge(webpackConfig, customConfig)
+
+module.exports = webpackConfig
